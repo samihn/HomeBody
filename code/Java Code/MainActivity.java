8af5694ba76c68d42d79f6bc325690a641286193
Kasey871 <hillk8@tcnj.edu>
2019-04-25 10:01:03 -0400

    Update MainActivity.java

--- code/Java Code/MainActivity.java
+++ code/Java Code/MainActivity.java
@@ -1,3 +1,5 @@
+/* review */
+
 // Project name: HomeBody
 // Description: An Android application where low-income individuals in the Ewing/Trenton area can locate gender inclusive housing nearby.
 // Filename: MainActivity.java
